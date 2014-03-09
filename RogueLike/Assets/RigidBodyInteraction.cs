21b50dd3344bc60c7bacbe159d0ab6afc7739b4c
bfelch <kagrenac@live.com>
2014-03-09 18:57:19 -0500

    The first of many?
    Hey, look, there's the start of a project here.  But not very much.

---
+++ RogueLike/Assets/RigidBodyInteraction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidBodyInteraction : MonoBehaviour {
+
+	float pushPower = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Rigidbody body = hit.collider.attachedRigidbody;
+
+		if (body == null || body.isKinematic)
+			return;
+
+		if (hit.moveDirection.y < -0.3)
+			return;
+
+		Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
+
+		body.velocity = pushDir * pushPower;
+	}
+}
